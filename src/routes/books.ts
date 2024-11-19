ac4d35e71408ed2c641cc88516203e51f008e6f2
fahrulrz <mfahrulrazi695@gmail.com>
2024-11-19 18:02:56 +0700

    s

--- src/routes/books.ts
+++ src/routes/books.ts
@@ -1,83 +1,40 @@
 import { Request, Response, Router } from "express";
 import { Book } from "../models/book";
-import { PrismaClient } from "@prisma/client";
-import { MongoClient } from "mongodb";
 
-
-const uri = "mongodb+srv://muhamadfahrulrazi:O6gnyJe0fRkY6tBM@cluster0.ep7kl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 const router = Router();
-const prisma = new PrismaClient();
-const client = new MongoClient(uri);
-
-async function run() {
-    try {
-        await client.connect();
-        await client.db("test").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-    }
-}
-
 
 let books: Book[] =[];
 let currentId = books.length;
 
 // Create book
-router.post("/", async(req: Request, res: Response) => {
+router.post("/", (req: Request, res: Response) => {
     const { title, author, published_at } = req.body;
 
     if (!title || !author || !published_at) {
         return res.status(400).json({ message: "Invalid request data" });
     }
 
-    try {
-        const newBook = await prisma.book.create({
-            data: {
-                title,
-                author,
-                published_at,
-                created_at: new Date().toISOString(),
-                updated_at: new Date().toISOString(),
-                id: currentId++,
-            },
-        })
+    const newBook: Book = {
+        title,
+        author,
+        published_at,
+        created_at: new Date().toISOString(),
+        updated_at: new Date().toISOString(),
+        id: currentId++,
+    };
 
-        res.status(201).json({
+    books.push(newBook);
+
+    res.status(201).json({
         message: "Book created successfully", 
         data: newBook });
-
-    } catch(error) {
-        res.status(500).json({ message: "Internal server error" });
-    }
-
-    // const newBook: Book = {
-    //     title,
-    //     author,
-    //     published_at,
-    //     created_at: new Date().toISOString(),
-    //     updated_at: new Date().toISOString(),
-    //     id: currentId++,
-    // };
-
-    // books.push(newBook);
-
-    // res.status(201).json({
-    //     message: "Book created successfully", 
-    //     data: newBook });
 })
 
 
 // Read all books
-router.get("/", async(req, res)=> {
-    try {
-        const books = await prisma.book.findMany();
-        res.status(200).json({ data: books })
-    } catch(error) {
-        res.status(500).json({ message: "Internal server error" });
-    }
+router.get("/", (req, res)=> {
+    res.status(200).json({ data: books })
 })
 
 // Read single book
